dbb736872eea14e72228d21972f7c25762a679ee
SergeevPavel4696 <97850194+SergeevPavel4696@users.noreply.github.com>
2022-10-08 14:15:30 +0300

    Add files via upload

---
+++ src/routes/cards.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+const {
+  createCard, addLike, deleteLike, getCard, getCards,
+} = require('../controllers/cards');
+
+router.post('/cards', createCard);
+router.put('/cards/:cardId/likes', addLike);
+router.delete('/cards/:cardId/likes', deleteLike);
+router.get('/cards/:cardId', getCard);
+router.get('/cards', getCards);
+
+module.exports = router;
