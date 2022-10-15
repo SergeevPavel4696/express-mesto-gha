ec9943912588c397cc7f2de775d16b25e760d875
SergeevPavel4696 <97850194+SergeevPavel4696@users.noreply.github.com>
2022-10-15 20:52:00 +0300

    Update users.js

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,7 +1,6 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
-const UnAuthorizedError = require('../errors/UnAuthorizedError');
 const NotFoundError = require('../errors/NotFoundError');
 const BadRequestError = require('../errors/BadRequestError');
 const AlreadyExistsError = require('../errors/AlreadyExistsError');
