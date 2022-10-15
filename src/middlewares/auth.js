6ff1b093a4e91438e326ca9ab64e0e198fb9d8de
SergeevPavel4696 <97850194+SergeevPavel4696@users.noreply.github.com>
2022-10-15 23:13:07 +0300

    Update auth.js

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -4,16 +4,15 @@ const UnAuthorizedError = require('../errors/UnAuthorizedError');
 const auth = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    next(new UnAuthorizedError('Необходимаавторизация'));
-  } else {
-    const token = authorization.replace('Bearer ', '');
-    try {
-      req.user = jwt.verify(token, 'some-secret-key');
-    } catch (err) {
-      next(new UnAuthorizedError('Необходима авторизация'));
-    }
-    next();
+    return next(new UnAuthorizedError('Необходимаавторизация'));
   }
+  const token = authorization.replace('Bearer ', '');
+  try {
+    req.user = jwt.verify(token, 'some-secret-key');
+  } catch (err) {
+    return next(new UnAuthorizedError('Необходима авторизация'));
+  }
+  return next();
 };
 
 module.exports = auth;
