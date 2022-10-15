985083184a53ca7428904dbbab3069e4bf9e27e7
SergeevPavel4696 <97850194+SergeevPavel4696@users.noreply.github.com>
2022-10-15 16:23:06 +0300

    Update app.js

--- app.js
+++ app.js
@@ -9,10 +9,6 @@ const errorHandler = require('./src/middlewares/errorHandler');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-app.use(errors);
-
-app.use(errorHandler);
-
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useUnifiedTopology: false,
@@ -39,5 +35,9 @@ app.use(require('./src/routes/users'));
 app.use(require('./src/routes/cards'));
 
 app.use('/', (req, res) => res.status(404).send({ message: 'Некорректный адрес запроса.' }));
+
+app.use(errors);
+
+app.use(errorHandler);
 
 app.listen(PORT);
