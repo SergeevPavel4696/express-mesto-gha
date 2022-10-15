9f6c948495d1647a3a78a0bf68ff630494f048e3
SergeevPavel4696 <97850194+SergeevPavel4696@users.noreply.github.com>
2022-10-15 21:46:56 +0300

    Update app.js

--- app.js
+++ app.js
@@ -17,12 +17,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-  }),
-}), login);
+app.post('/signin', login);
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
