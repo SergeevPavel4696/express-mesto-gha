01f2c9fa113ec8ac677fe66e54e44277579b980c
SergeevPavel4696 <97850194+SergeevPavel4696@users.noreply.github.com>
2022-10-15 16:50:41 +0300

    Update user.js

--- src/models/user.js
+++ src/models/user.js
@@ -2,13 +2,6 @@ const mongoose = require('mongoose');
 
 const validator = require('validator');
 
-const linkRegex = /^https?:\/\/(www.)?[a-z0-9.-]{2,}\.[a-z]{2,}\/?[-._~:/?#[\]@!$&'()*+,;=\w]*#?$/;
-
-function linkValidator(link) {
-  const regex = new RegExp(linkRegex);
-  return regex.test(link);
-}
-
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -24,10 +17,6 @@ const userSchema = new mongoose.Schema({
   },
   avatar: {
     type: String,
-    validate: {
-      validator: linkValidator,
-      message: (props) => `${props.value} is not a valid link`,
-    },
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
   email: {
