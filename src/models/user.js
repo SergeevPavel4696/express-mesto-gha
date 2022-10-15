18c172cf7f01bb15bf2fecc2e67942444c7bad17
SergeevPavel4696 <97850194+SergeevPavel4696@users.noreply.github.com>
2022-10-15 17:40:41 +0300

    Update user.js

--- src/models/user.js
+++ src/models/user.js
@@ -5,25 +5,41 @@ const validator = require('validator');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
+    required: false,
     minlength: 2,
     maxlength: 30,
     default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
+    required: false,
     minlength: 2,
     maxlength: 30,
     default: 'Исследователь',
   },
   avatar: {
     type: String,
+    required: false,
+    validate: {
+      validator(value) {
+        // eslint-disable-next-line
+        const regexp = /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
+        return regexp.test(value);
+      },
+      message: 'Некорректный адрес.',
+    },
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
   email: {
     type: String,
     required: true,
     unique: true,
-    validate: validator.isEmail,
+    validate: {
+      validator(value) {
+        return validator.isEmail(value);
+      },
+      message: 'Некорректный email.',
+    },
   },
   password: {
     type: String,
