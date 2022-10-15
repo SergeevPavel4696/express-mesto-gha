8b290c5b36b0503e0140f3621a26b18b74387f97
SergeevPavel4696 <97850194+SergeevPavel4696@users.noreply.github.com>
2022-10-15 21:34:30 +0300

    Update auth.js

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,10 +1,11 @@
 const jwt = require('jsonwebtoken');
 const UnAuthorizedError = require('../errors/UnAuthorizedError');
+const BadRequestError = require('../errors/BadRequestError');
 
 const auth = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    next(new UnAuthorizedError('Необходима авторизация'));
+    next(new BadRequestError('Необходима авторизация'));
   } else {
     const token = authorization.replace('Bearer ', '');
     try {
