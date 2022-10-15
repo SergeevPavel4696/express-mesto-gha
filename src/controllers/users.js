6a497ad8f5848cc06ba7a42db5162461aebe9881
SergeevPavel4696 <97850194+SergeevPavel4696@users.noreply.github.com>
2022-10-15 20:49:17 +0300

    Update users.js

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -114,7 +114,7 @@ const login = (req, res, next) => {
               const token = jwt.sign(_id, 'some-secret-key', { expiresIn: 3600 * 24 * 7 });
               res.send({ token });
             } else {
-              Promise.reject(new UnAuthorizedError('Неправильные почта или пароль.'));
+              Promise.reject(new BadRequestError('Неправильные почта или пароль.'));
             }
           });
       } else {
