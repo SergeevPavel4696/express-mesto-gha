ac2b60f159b13377e3cf07533336d125f07026c9
SergeevPavel4696 <97850194+SergeevPavel4696@users.noreply.github.com>
2022-10-17 18:01:44 +0300

    Add files via upload

--- src/routes/cards.js
+++ src/routes/cards.js
@@ -9,7 +9,7 @@ const {
 router.post('/', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().pattern(/https?:\/\/(w{3})?[a-z0-9-]+\.[a-z0-9\S]{2,}/),
+    link: Joi.string().required().pattern(/https?:\/\/(w{3})?[a-z0-9-]+\.[a-z0-9\S]{2,}/),
   }),
 }), createCard);
 
--- src/routes/users.js
+++ src/routes/users.js
@@ -6,8 +6,17 @@ const {
   updateUserInfo, updateUserAvatar, getUser, getUsers, getMe,
 } = require('../controllers/users');
 
-router.patch('/me', updateUserInfo);
-router.patch('/me/avatar', updateUserAvatar);
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
+  }),
+}), updateUserInfo);
+router.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().pattern(/https?:\/\/(w{3})?[a-z0-9-]+\.[a-z0-9\S]{2,}/),
+  }),
+}), updateUserAvatar);
 
 router.get('/', getUsers);
 router.get('/me', getMe);
