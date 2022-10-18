8a53827eee1cf211a54203a7f2c893cda5de6bcf
SergeevPavel4696 <97850194+SergeevPavel4696@users.noreply.github.com>
2022-10-18 22:48:44 +0300

    Update cards.js

--- src/controllers/cards.js
+++ src/controllers/cards.js
@@ -29,9 +29,10 @@ const deleteCard = (req, res, next) => {
         card.remove()
           .then(() => {
             res.send(card);
+          })
+          .catch(() => {
+            next(new ForbiddenError('Вы не можете удалить чужую карточку.'));
           });
-      } else {
-        throw new ForbiddenError('Вы не можете удалить чужую карточку.');
       }
     })
     .catch((err) => {
