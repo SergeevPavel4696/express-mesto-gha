3e1219c4dee4341abdea1ff7dc037ef492c9a5a1
SergeevPavel4696 <97850194+SergeevPavel4696@users.noreply.github.com>
2022-10-28 17:59:32 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -16,9 +16,33 @@ const { requestLogger, errorLogger } = require('./src/middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+const allowedCors = [
+  'http://sergeevpavel.mesto.nomoredomains.icu/',
+  'https://sergeevpavel.mesto.nomoredomains.icu/',
+  'localhost:3000',
+];
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+  }
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+  return next();
+});
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
