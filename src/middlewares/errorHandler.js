8ba33efefc1575a5018c4b0c2b049c7d16fa9158
SergeevPavel4696 <97850194+SergeevPavel4696@users.noreply.github.com>
2022-11-13 17:10:03 +0300

    Update errorHandler.js

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,6 +1,6 @@
 const errorHandler = (err, req, res, next) => {
   const { statusCode = 500, message } = err;
-  res.status(statusCode).send({ message: statusCode === 500 ? 'Сервер не работает' : message });
+  res.status(statusCode).send({ message: statusCode === 500 ? err : message });
   next();
 };
 
