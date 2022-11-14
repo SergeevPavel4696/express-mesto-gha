6fe7c7ea4f4216b07bde33ca8098cdd04982d66e
SergeevPavel4696 <97850194+SergeevPavel4696@users.noreply.github.com>
2022-11-14 13:45:34 +0300

    Add files via upload

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,6 +1,17 @@
+const allowedCors = [
+  'sergeevpavel.mesto.nomoredomains.icu',
+  'http://sergeevpavel.mesto.nomoredomains.icu',
+  'https://sergeevpavel.mesto.nomoredomains.icu',
+  'http://localhost:3000',
+  'https://localhost:3000',
+];
+
 const cors = (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', "*");
-  res.header('Access-Control-Allow-Credentials', true);
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+  }
   const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
