f5da5c0299d6e08b99ca3a66429ab75019c9e3cc
SergeevPavel4696 <97850194+SergeevPavel4696@users.noreply.github.com>
2022-11-15 18:37:20 +0300

    Update logger.js

--- src/middlewares/logger.js
+++ src/middlewares/logger.js
@@ -2,12 +2,12 @@ const expressWinston = require('express-winston');
 const winston = require('winston');
 
 const requestLogger = expressWinston.logger({
-  transports: [new winston.transports.File({ filename: 'request.log' })],
+  transports: [new winston.transports.File({ filename: 'request.json' })],
   format: winston.format.json(),
 });
 
 const errorLogger = expressWinston.errorLogger({
-  transports: [new winston.transports.File({ filename: 'error.log' })],
+  transports: [new winston.transports.File({ filename: 'error.json' })],
   format: winston.format.json(),
 });
 
